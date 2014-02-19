e5a84971f5ed7bbbd100ac15f035687f5c3c7545
aksyanov <aksyanov.r@gmail.com>
2014-02-20 01:09:11 +0300

    add about us

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,22 +1,22 @@
 <div id="i_content">
     <div id="i_main">
         <div id="i_topRects">
-            <div class="c_topRect" id="i_topRect_1">1</div>
-            <div class="c_topRectNonHover" id="i_topRect_2">2
+            <div class="c_topRect" id="i_topRect_1"></div>
+            <div class="c_topRectNonHover" id="i_topRect_2">
                 <img height="50px" width="50px" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo.png" id="i_mainLogoRect"/>
                 <div class="c_textRotateRect">DevTown</div>
             </div>
-            <div class="c_topRect" id="i_topRect_3">3</div>
-            <div class="c_topRect" id="i_topRect_4">4</div>
-            <div class="c_topRect" id="i_topRect_5">5</div>
-            <div class="c_topRect" id="i_topRect_6">6</div>
-            <div class="c_topRect" id="i_topRect_7">7</div>
-            <div class="c_topRect" id="i_topRect_8">8</div>
-            <div class="c_topRect" id="i_topRect_9">9</div>
-            <div class="c_topRectNonHover" id="i_topRect_10">10</div>
-            <div class="c_topRectNonHover" id="i_topRect_11">11</div>
-            <div class="c_topRectNonHover" id="i_topRect_12">12</div>
-            <div class="c_topRectNonHover" id="i_topRect_13">13</div>
+            <div class="c_topRect" id="i_topRect_3"></div>
+            <div class="c_topRect" id="i_topRect_4"></div>
+            <div class="c_topRect" id="i_topRect_5"></div>
+            <div class="c_topRect" id="i_topRect_6"></div>
+            <div class="c_topRect" id="i_topRect_7"></div>
+            <div class="c_topRect" id="i_topRect_8"></div>
+            <div class="c_topRect" id="i_topRect_9"></div>
+            <div class="c_topRectNonHover" id="i_topRect_10"></div>
+            <div class="c_topRectNonHover" id="i_topRect_11"></div>
+            <div class="c_topRectNonHover" id="i_topRect_12"></div>
+            <div class="c_topRectNonHover" id="i_topRect_13"></div>
         </div>
     </div>
     <div id="i_buttonMenuRect" class="c_corpBackgroungColor">
@@ -26,7 +26,57 @@
     <div id="i_services">
         <div id="i_servicesContent">
             <div class="c_contentHeader c_fontSize30">
-                Что <span class="c_corpTextColor">мы</span> производим<br>
+                Что <span class="c_corpTextColor">мы</span> производим
+            </div>
+            <br><br>
+            <div class="c_width800Min c_marginCenter c_contentTextMedium c_textCenter">
+                Здесь представлены наши <span class="c_corpTextColor">лучшие</span> работы в сфере разработки ПО.
+                <br><br>
+                Попробуйте посотреть все счетчки <span class="c_corpTextColor">работ</span> и кликнув на них увидеть <span class="c_corpTextColor">полное портфолио</span> работ в этой сфере.
+            </div>
+            <div id="i_servicesRectBlock" class="c_corpTextColor">
+                <div class="c_servicesRect floatLeft" style="margin-left: 20px">
+                    <div class="c_textRotateRectHor c_textCenter">
+                        <span class="c_contentTextLarge">572</span>
+                        <br>
+                        Web сайты
+                    </div>
+                </div>
+                <div class="c_servicesRect floatLeft">
+                    <div class="c_textRotateRectHor c_textCenter">
+                        <span class="c_contentTextLarge">231</span>
+                        <br>
+                        Мобильные
+                    </div>
+                </div>
+                <div class="c_servicesRect floatLeft">
+                    <div class="c_textRotateRectHor c_textCenter">
+                        <span class="c_contentTextLarge">312</span>
+                        <br>
+                        SEO
+                    </div>
+                </div>
+                <div class="c_servicesRect floatLeft">
+                    <div class="c_textRotateRectHor c_textCenter">
+                        <span class="c_contentTextLarge">126</span>
+                        <br>
+                        Статей
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="i_moreBlock">
+        <div id="i_moreBlockContent">
+            <div class="c_contentHeader c_fontSize30">
+                Немного о <span class="c_corpTextColor">нас</span>
+            </div>
+            <br><br>
+            <div class="c_width800Min c_marginCenter c_contentTextMedium c_textCenter">
+                Здесь представлены наши <span class="c_corpTextColor">лучшие</span> работы в сфере разработки ПО.
+                <br><br>
+                Попробуйте посотреть все счетчки <span class="c_corpTextColor">работ</span> и кликнув на них увидеть <span class="c_corpTextColor">полное портфолио</span> работ в этой сфере.
             </div>
         </div>
     </div>
