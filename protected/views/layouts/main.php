20ca20d6cb35855c6990ee12d0d5d70c2b232405
Ruslan <aksyanov.r@gmail.com>
2014-02-19 17:28:18 +0400

    add new elements

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/style/general.css">
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/style/main.css">
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/style/theme.css">
@@ -11,8 +15,8 @@
 <body>
     <?php echo $content; ?>
 
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquerycolor.js"></script>
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/main.js"></script>
+<!--    <script src="--><?php //echo Yii::app()->request->baseUrl; ?><!--/js/jquerycolor.js"></script>-->
+<!--    <script src="--><?php //echo Yii::app()->request->baseUrl; ?><!--/js/main.js"></script>-->
 
 
 </body>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,2 +1,19 @@
+<div id="main">
+    <div class="rect" id="r1">1</div>
+    <div class="rectBig" id="r2">
+        2
+        <img height="50px" width="50px" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo.png" id="mainLogo"/>
+        <div class="textRotate">DevTown</div>
+    </div>
+    <div class="rect" id="r3">3</div>
+    <div class="rect" id="r4">4</div>
+    <div class="rect" id="r5">5</div>
+    <div class="rect" id="r6">6</div>
 
+
+</div>
+
+<div style="height: 10100px">
+    asd
+</div>
 <?php //$this->widget('WServicesSlider'); ?>
