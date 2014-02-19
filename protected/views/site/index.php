65df024ed23a39f0796398140877c53587245384
Ruslan <aksyanov.r@gmail.com>
2014-02-19 18:16:27 +0400

    code review

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,16 +1,20 @@
-<div id="main">
-    <div class="rect" id="r1">1</div>
-    <div class="rectBig" id="r2">
-        2
-        <img height="50px" width="50px" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo.png" id="mainLogo"/>
-        <div class="textRotate">DevTown</div>
+<div id="i_content">
+    <div id="i_main">
+        <div id="i_topRects">
+            <div class="c_topRect" id="i_topRect_1"></div>
+            <div class="c_topRectNonHover" id="i_topRect_2">
+                <img height="50px" width="50px" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo.png" id="i_mainLogoRect"/>
+                <div class="c_textRotateRect">DevTown</div>
+            </div>
+            <div class="c_topRect" id="i_topRect_3"></div>
+            <div class="c_topRect" id="i_topRect_4"></div>
+            <div class="c_topRect" id="i_topRect_5"></div>
+            <div class="c_topRect" id="i_topRect_6"></div>
+        </div>
     </div>
-    <div class="rect" id="r3">3</div>
-    <div class="rect" id="r4">4</div>
-    <div class="rect" id="r5">5</div>
-    <div class="rect" id="r6">6</div>
-
-
+    <div id="i_buttonMenuRect">
+        <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icon/eye.png" style="-webkit-transform:rotate(-45deg);margin-top: 8px" width="55px"/>
+    </div>
 </div>
 
 <div style="height: 10100px">
